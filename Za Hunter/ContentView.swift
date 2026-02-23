bafa8e7b9fcbb95735e8c5a7bb4e2da2ceda8dd1
Joseph <jjphillipse4@gmail.com>
2026-02-23 11:00:58 -0600

    i need help

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -1,28 +1,53 @@
-//
-//  ContentView.swift
-//  Za Hunter
-//
-//  Created by joseph phillips on 2/17/26.
-//
-
 import SwiftUI
 import MapKit
 
 struct ContentView: View {
     @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
-    @State private var mapRegion = MKCoordinateRegion ()
+    @State private var mapRegion = MKCoordinateRegion()
+    @State private var locationManager = LocationManager()
+    @State private var places = [Place]()
     var body: some View {
-        
         NavigationView {
             Map(position: $startPosition) {
-                UserAnnotation ()
+                UserAnnotation()
+                ForEach(places) { place in
+                    Annotation(place.mapItem.name!, coordinate: place.mapItem.placemark.coordinate) {
+                        NavigationLink(destination: LocationDetailsView(mapItem: place.mapItem)) {
+                            Image("Pizza")
+                        }
+                    }
+                }
+            }
+            .onMapCameraChange { context in
+                mapRegion = context.region
+                performSearch(item: "Pizza")
             }
             .navigationBarTitle("'Za Hunter", displayMode: .inline)
             .toolbarBackground(.hidden, for: .navigationBar)
+        }
+    }
+    
+    func performSearch(item: String) {
+        let searchRequest = MKLocalSearch.Request ()
+        searchRequest.naturalLanguageQuery = item
+        searchRequest.region = mapRegion
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { response, error in
+            if let response = response {
+                places.removeAll()
+                for mapItem in response.mapItems {
+                    places.append(Place(mapItem: mapItem))
+                }
+            }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Place: Identifiable {
+    let id = UUID()
+    let mapItem: MKMapItem
 }
---
+++ Za Hunter/SwiftUIView.swift
@@ -0,0 +1,14 @@
+import Foundation
+import CoreLocation
+
+@Observable
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    
+    override init() {
+        super.init ()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+}
