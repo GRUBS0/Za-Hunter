5bd447ef898776434e448bbe14d843c5e47c9f6f
Joseph <jjphillipse4@gmail.com>
2026-02-24 10:30:23 -0600

    done

--- Za Hunter/ContentView.swift
+++ Za Hunter/ContentView.swift
@@ -1,6 +1,5 @@
 import SwiftUI
 import MapKit
-
 struct ContentView: View {
     @State private var startPosition = MapCameraPosition.userLocation(fallback: .automatic)
     @State private var mapRegion = MKCoordinateRegion()
@@ -13,41 +12,38 @@ struct ContentView: View {
                 ForEach(places) { place in
                     Annotation(place.mapItem.name!, coordinate: place.mapItem.placemark.coordinate) {
                         NavigationLink(destination: LocationDetailsView(mapItem: place.mapItem)) {
-                            Image("Pizza")
+                            Image("pizza")
                         }
                     }
                 }
             }
-            .onMapCameraChange { context in
-                mapRegion = context.region
-                performSearch(item: "Pizza")
+                .onMapCameraChange { context in
+                    mapRegion = context.region
+                    performSearch(item: "pizza")
+                }
             }
-            .navigationBarTitle("'Za Hunter", displayMode: .inline)
-            .toolbarBackground(.hidden, for: .navigationBar)
         }
-    }
-    
-    func performSearch(item: String) {
-        let searchRequest = MKLocalSearch.Request ()
-        searchRequest.naturalLanguageQuery = item
-        searchRequest.region = mapRegion
-        let search = MKLocalSearch(request: searchRequest)
-        search.start { response, error in
-            if let response = response {
-                places.removeAll()
-                for mapItem in response.mapItems {
-                    places.append(Place(mapItem: mapItem))
+        
+        func performSearch(item: String) {
+            let searchRequest = MKLocalSearch.Request()
+            searchRequest.naturalLanguageQuery = item
+            searchRequest.region = mapRegion
+            let search = MKLocalSearch(request: searchRequest)
+            
+            search.start { response, error in
+                if let response = response {
+                    places.removeAll()
+                    for mapItem in response.mapItems {
+                        places.append(Place(mapItem: mapItem))
+                    }
                 }
             }
         }
     }
-}
-
-#Preview {
-    ContentView()
-}
-
-struct Place: Identifiable {
-    let id = UUID()
-    let mapItem: MKMapItem
-}
+    #Preview {
+        ContentView()
+    }
+    struct Place: Identifiable {
+        let id = UUID()
+        let mapItem: MKMapItem
+    }
--- Za Hunter/LocationManager2.swift
+++ Za Hunter/LocationManager2.swift
@@ -1,12 +1,11 @@
 import Foundation
 import CoreLocation
-
 @Observable
 class LocationManager: NSObject, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     
     override init() {
-        super.init ()
+        super.init()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
